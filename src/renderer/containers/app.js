ebe53c5b5861f7eb0274947db7a45d65a45d2488
fiahfy <d.fiahfy@gmail.com>
2016-06-23 22:49:27 +0900

    Update

--- src/renderer/containers/app.js
+++ src/renderer/containers/app.js
@@ -1,12 +1,14 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import {RaisedButton, Toolbar, ToolbarGroup} from 'material-ui'
+import {RaisedButton, Toolbar, ToolbarGroup,
+Drawer, MenuItem} from 'material-ui'
 // const injectTapEventPlugin = require("react-tap-event-plugin")
 // injectTapEventPlugin()
 import * as ActionCreators from '../actions'
 import HostList from '../components/host-list'
 import HostDialog from '../components/host-dialog'
+import {remote} from 'electron'
 
 function mapStateToProps(state) {
   return {hosts: state.hosts}
@@ -50,12 +52,45 @@ export default class App extends Component {
     this.refs.hostList.unselect()
     this.props.actions.deleteHosts(indexes)
   }
+  componentDidMount() {
+    const menu = new remote.Menu();
+menu.append(new remote.MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked'); }}));
+menu.append(new remote.MenuItem({type: 'separator'}));
+menu.append(new remote.MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}));
+
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  menu.popup(remote.getCurrentWindow());
+}, false);
+  }
   render() {
     const {hosts} = this.props
     const {open} = this.state
 
     return (
       <div style={styles.app}>
+        <Drawer open={true}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 3</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
         <HostDialog
           ref="hostDialog"
           open={open}
@@ -70,12 +105,12 @@ export default class App extends Component {
             onEditHost={::this.handleEditHost}
           />
         </div>
-        <Toolbar style={styles.toolbar}>
+        {/*<Toolbar style={styles.toolbar}>
           <ToolbarGroup firstChild={true}>
             <RaisedButton label="Add" onClick={::this.handleOpenDialog} primary={true} style={styles.button} />
             <RaisedButton label="Delete" onClick={::this.handleDeleteHosts} secondary={true} style={styles.button} />
           </ToolbarGroup>
-        </Toolbar>
+        </Toolbar>*/}
       </div>
     )
   }
@@ -83,15 +118,16 @@ export default class App extends Component {
 
 const styles = {
   app: {
-    WebkitUserSelect: 'none',
-    paddingBottom: 56,
+    // WebkitUserSelect: 'none',
+    // paddingBottom: 56,
     overflow: 'hidden',
     height: '100%',
     boxSizing: 'border-box'
   },
   container: {
     overflow: 'auto',
-    height: '100%'
+    height: '100%',
+    paddingLeft: '256px'
   },
   button: {
     margin: 12
