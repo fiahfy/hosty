cfecf6a983221c4ffa2bfcaa7b7ddd0b651c1f0a
fiahfy <d.fiahfy@gmail.com>
2016-07-16 15:39:29 +0900

    Change icon

--- bin/create_icons.sh
+++ bin/create_icons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SIZES="16 32 128 256"
-ORG_SIZE="512"
+SIZES="16 32 64 128 256 512"
+ORG_SIZE="1024"
 ORG_FILE="icon_${ORG_SIZE}x${ORG_SIZE}.png"
 
 cd $(dirname $0)
@@ -14,7 +14,25 @@ do
   echo "${FILE} is created"
 done
 
+for SIZE in $SIZES
+do
+  FILE="icon_${SIZE}x${SIZE}@2x.png"
+  sips -Z $((${SIZE} * 2)) ${ORG_FILE} --out ${FILE}
+  echo "${FILE} is created"
+done
+
 cd ..
 
 iconutil -c icns hosty.iconset
 echo "icns file is created"
+
+ARGS=""
+SIZES="${SIZES} ${ORG_SIZE}"
+for SIZE in $SIZES
+do
+  FILE="icon_${SIZE}x${SIZE}.png"
+  ARGS="${ARGS} hosty.iconset/${FILE}"
+done
+
+convert ${ARGS} hosty.ico
+echo "ico file is created"
