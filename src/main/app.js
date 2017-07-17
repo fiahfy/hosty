9ab8642989d9adacf11a4de29b388017a5e982ee
fiahfy <d.fiahfy@gmail.com>
2017-07-17 20:56:14 +0900

    Add electron-window-state

--- src/main/app.js
+++ src/main/app.js
@@ -1,10 +1,6 @@
 import { app } from 'electron';
-import fs from 'fs';
-import path from 'path';
 import Window from './window';
 import * as HostsFileManager from '../renderer/utils/hosts-file-manager';
-
-const PATH_WINDOW_OPTIONS = path.join(app.getPath('userData'), 'window.json');
 
 export default class App {
   load() {
@@ -15,27 +11,11 @@ export default class App {
       return;
     }
 
-    const options = App.loadWindowOptions();
-
     this.window = new Window(this);
-    this.window.open(options);
+    this.window.open();
   }
   removeWindow() {
     this.window = null;
-  }
-  static loadWindowOptions() {
-    try {
-      return JSON.parse(fs.readFileSync(PATH_WINDOW_OPTIONS));
-    } catch (e) {
-      return null;
-    }
-  }
-  static saveWindowOptions(options) {
-    try {
-      fs.writeFileSync(PATH_WINDOW_OPTIONS, JSON.stringify(options));
-    } catch (e) {
-      //
-    }
   }
   handleEvents() {
     app.on('ready', () => {
--- src/main/window.js
+++ src/main/window.js
@@ -1,15 +1,21 @@
 import { BrowserWindow } from 'electron';
+import windowStateKeeper from 'electron-window-state';
 import MenuBuilder from './menu-builder';
 
 export default class Window {
   constructor(application) {
     this.application = application;
   }
-  open(args) {
-    const options = args || { width: 820, height: 600 };
+  open() {
+    const windowState = windowStateKeeper({
+      defaultWidth: 820,
+      defaultHeight: 600,
+    });
 
-    this.browserWindow = new BrowserWindow(options);
+    this.browserWindow = new BrowserWindow(windowState);
     this.browserWindow.loadURL(`file://${__dirname}/app/index.html`);
+
+    windowState.manage(this.browserWindow);
 
     const builder = new MenuBuilder(this.browserWindow);
     builder.build();
@@ -17,11 +23,6 @@ export default class Window {
     this.handleEvents();
   }
   handleEvents() {
-    this.browserWindow.on('close', () => {
-      const options = this.browserWindow.getBounds();
-      this.application.constructor.saveWindowOptions(options);
-    });
-
     this.browserWindow.on('closed', () => {
       this.browserWindow = null;
       this.application.removeWindow();
