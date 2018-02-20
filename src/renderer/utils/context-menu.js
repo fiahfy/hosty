ac02dc778f76177b4bbe4db0b1067b1927b830c4
fiahfy <d.fiahfy@gmail.com>
2018-02-20 17:43:06 +0900

    Refactor context menu

---
+++ src/renderer/utils/context-menu.js
@@ -0,0 +1,61 @@
+import { remote } from 'electron'
+
+const { Menu } = remote
+
+const appendInspectElementMenu = (e, template) => {
+  const { clientX: x, clientY: y } = e
+
+  if (template.length) {
+    template = template.concat([{ type: 'separator' }])
+  }
+  return template.concat([{
+    label: 'Inspect Element',
+    click: () => {
+      remote.getCurrentWindow().inspectElement(x, y)
+    }
+  }])
+}
+
+export const Label = {
+  Cut: 'Cut',
+  Copy: 'Copy',
+  Paste: 'Paste'
+}
+
+export const show = (e, template = []) => {
+  e.preventDefault()
+  e.stopPropagation()
+
+  if (process.env.NODE_ENV !== 'production') {
+    template = appendInspectElementMenu(e, template)
+  }
+
+  template = template.map((item) => {
+    switch (item.label) {
+      case Label.cut:
+        return {
+          label: item.label,
+          click: () => document.execCommand('cut'),
+          accelerator: 'CmdOrCtrl+X'
+        }
+      case Label.copy:
+        return {
+          label: item.label,
+          click: () => document.execCommand('copy'),
+          accelerator: 'CmdOrCtrl+C'
+        }
+      case Label.paste:
+        return {
+          label: item.label,
+          click: () => document.execCommand('paste'),
+          accelerator: 'CmdOrCtrl+V'
+        }
+      default:
+        return item
+    }
+  })
+
+  Menu
+    .buildFromTemplate(template)
+    .popup(remote.getCurrentWindow(), { async: true })
+}
