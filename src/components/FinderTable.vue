91dfbe8dbfd6cfc987244780b4ed96e43afa2436
fiahfy <d.fiahfy@gmail.com>
2018-07-20 16:57:08 +0900

    Use virtual table

--- src/components/FinderTable.vue
+++ src/components/FinderTable.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-data-table
+  <virtual-data-table
     ref="table"
     :headers="headers"
     :items="filteredItems"
-    :disable-initial-sort="true"
-    :class="classes"
     class="finder-table"
     item-key="id"
     no-data-text="No results"
     hide-actions
+    sticky-headers
     tabindex="0"
-    @keydown.native="onKeyDown"
+    @scroll="onScroll"
     @click.native="onClick"
+    @keydown.native="onKeyDown"
   >
     <template
       slot="headers"
@@ -24,22 +24,24 @@
       slot-scope="props"
     >
       <finder-table-row
-        :ref="`row-${props.item.id}`"
+        :key="props.item.id"
         :item="props.item"
       />
     </template>
-  </v-data-table>
+  </virtual-data-table>
 </template>
 
 <script>
 import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 import FinderTableHeaderRow from './FinderTableHeaderRow'
 import FinderTableRow from './FinderTableRow'
+import VirtualDataTable from './VirtualDataTable'
 
 export default {
   components: {
     FinderTableHeaderRow,
-    FinderTableRow
+    FinderTableRow,
+    VirtualDataTable
   },
   data () {
     return {
@@ -66,11 +68,6 @@ export default {
     }
   },
   computed: {
-    classes () {
-      return {
-        scrolling: this.scrolling
-      }
-    },
     ...mapState('local/finder', [
       'selectedItemId',
       'scrollTop'
@@ -94,33 +91,30 @@ export default {
           offsetHeight: rowHeight
         }
         const table = {
-          scrollTop: this.container.scrollTop,
-          offsetHeight: this.container.offsetHeight
+          scrollTop: this.$refs.table.getScrollTop(),
+          offsetHeight: this.$refs.table.getOffsetHeight()
         }
         if (el.offsetTop - el.offsetHeight < table.scrollTop) {
-          this.container.scrollTop = el.offsetTop - el.offsetHeight
+          this.$refs.table.setScrollTop(el.offsetTop - el.offsetHeight)
         } else if (el.offsetTop + headerHeight > table.scrollTop + table.offsetHeight) {
-          this.container.scrollTop = el.offsetTop + headerHeight - table.offsetHeight
+          this.$refs.table.setScrollTop(el.offsetTop + headerHeight - table.offsetHeight)
         }
       })
     }
   },
   mounted () {
     this.loadItems()
-    this.container = this.$el.querySelector('.v-table__overflow')
-    this.container.addEventListener('scroll', this.onScroll)
     const scrollTop = this.scrollTop
     this.$nextTick(() => {
-      this.container.scrollTop = scrollTop
+      this.$refs.table.setScrollTop(scrollTop)
     })
-  },
-  beforeDestroy () {
-    this.container.removeEventListener('scroll', this.onScroll)
   },
   methods: {
     onScroll (e) {
-      this.scrolling = e.target.scrollTop > 0
       this.setScrollTop({ scrollTop: e.target.scrollTop })
+    },
+    onClick () {
+      this.unselectItem()
     },
     onKeyDown (e) {
       switch (e.keyCode) {
@@ -145,9 +139,6 @@ export default {
           break
       }
     },
-    onClick () {
-      this.unselectItem()
-    },
     ...mapMutations('local/finder/', [
       'setScrollTop'
     ]),
@@ -166,42 +157,8 @@ export default {
 <style scoped lang="scss">
 .finder-table {
   outline: none;
-  & /deep/ .v-table__overflow {
-    height: 100%;
-    overflow-y: scroll;
-    .v-datatable {
-      table-layout: fixed;
-      &>thead {
-        background: inherit;
-        &>tr {
-          background: inherit;
-          &>th {
-            background: inherit;
-            position: sticky;
-            top: 0;
-            z-index: 1;
-          }
-          &.v-datatable__progress>th {
-            top: 56px;
-            z-index: 0;
-            &:after {
-              bottom: 0;
-              box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);
-              content: '';
-              left: 0;
-              position: absolute;
-              width: 100%;
-            }
-          }
-        }
-      }
-    }
-  }
-  &.scrolling /deep/ .v-datatable>thead>tr {
-    border-bottom: none;
-    &.v-datatable__progress>th:after {
-      height: 10px;
-    }
+  & /deep/ .v-datatable {
+    table-layout: fixed;
   }
 }
 </style>
