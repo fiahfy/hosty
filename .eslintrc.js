275e11738cc0c97daabb860aaeafc22bb12792da
fiahfy <d.fiahfy@gmail.com>
2018-10-23 09:01:49 +0900

    Fix icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:vue/recommended'
   ]
 }
