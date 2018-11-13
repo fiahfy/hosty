58d032484594d6ea46936cce1b68f11f82b39bbb
fiahfy <d.fiahfy@gmail.com>
2018-11-14 08:41:23 +0900

    Add shadow on scrolling

--- src/components/SearchResultTreeview.vue
+++ src/components/SearchResultTreeview.vue
@@ -1,43 +1,54 @@
 <template>
   <v-container
-    id="scroll-target"
-    ref="treeview"
-    class="search-result-treeview scroll-y pa-0"
+    class="search-result-treeview pa-0"
     fluid
   >
-    <v-layout v-if="results.length">
-      <v-treeview
-        v-scroll:#scroll-target="onScroll"
-        class="spacer"
-        item-children="hosts"
-        item-key="key"
-        item-text="text"
-        open-all
-        open-on-click
-        activatable
-        hoverable
-        :items="results"
-        :active.sync="active"
-      >
-        <template
-          slot="prepend"
-          slot-scope="{ item }"
-        >
-          <span
-            v-if="item.disabled"
-            class="dummy-icon"
-          />
-          <v-icon v-else>check_circle</v-icon>
-        </template>
-      </v-treeview>
-    </v-layout>
-    <v-layout
-      v-else
-      align-center
-      justify-center
+    <v-container
+      id="scroll-target"
+      ref="treeview"
+      class="pa-0"
+      fluid
+      fill-height
+      scroll-y
     >
-      <v-flex class="text-xs-center caption">No results found</v-flex>
-    </v-layout>
+      <v-layout v-if="results.length">
+        <div
+          v-if="scrolling"
+          class="shadow"
+        />
+        <v-treeview
+          v-scroll:#scroll-target="onScroll"
+          class="spacer"
+          item-children="hosts"
+          item-key="key"
+          item-text="text"
+          open-all
+          open-on-click
+          activatable
+          hoverable
+          :items="results"
+          :active.sync="active"
+        >
+          <template
+            slot="prepend"
+            slot-scope="{ item }"
+          >
+            <span
+              v-if="item.disabled"
+              class="dummy-icon"
+            />
+            <v-icon v-else>check_circle</v-icon>
+          </template>
+        </v-treeview>
+      </v-layout>
+      <v-layout
+        v-else
+        align-center
+        justify-center
+      >
+        <v-flex class="text-xs-center body-1">No results found</v-flex>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
@@ -47,6 +58,7 @@ import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 export default {
   data() {
     return {
+      scrolling: false,
       active: []
     }
   },
@@ -61,6 +73,11 @@ export default {
       }
       const key = value[0]
       this.viewResult({ key })
+    },
+    results(value) {
+      if (!value.length) {
+        this.scrolling = 0
+      }
     }
   },
   mounted() {
@@ -72,7 +89,9 @@ export default {
   },
   methods: {
     onScroll(e) {
-      this.setScrollTop({ scrollTop: e.target.scrollTop })
+      const scrollTop = e.target.scrollTop
+      this.setScrollTop({ scrollTop })
+      this.scrolling = scrollTop > 0
     },
     ...mapMutations('local/search/', ['setScrollTop']),
     ...mapActions('local/search/', ['viewResult'])
@@ -82,6 +101,16 @@ export default {
 
 <style scoped lang="scss">
 .search-result-treeview {
+  position: relative;
+  .shadow {
+    box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2),
+      0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
+    content: '';
+    height: 10px;
+    position: absolute;
+    top: -10px;
+    width: 100%;
+  }
   .dummy-icon {
     width: 24px;
   }
