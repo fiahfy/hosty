c2e611d09a1e1098eb5aa734cadfd042aaf8c3b2
fiahfy <d.fiahfy@gmail.com>
2018-11-27 13:37:25 +0900

    yarn upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     'plugin:vue/recommended'
   ],
   rules: {
-    'vue/max-attributes-per-line': 'off'
+    'vue/max-attributes-per-line': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
   }
 }
